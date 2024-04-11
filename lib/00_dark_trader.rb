2cd9eb375496eb5e6f867f41d5f02b04c82b78c7
valentin-orrit-wsl <vorrit@gmail.com>
2024-04-11 15:38:20 +0200

    modifs

---
+++ lib/00_dark_trader.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+begin
+  doc = Nokogiri::HTML5(URI.open("https://coinmarketcap.com/all/views/all/"))
+
+  cryptos = {}
+  cryptos_array = []
+  print "loading."
+  doc.css("tr.cmc-table-row").each do |crypto|
+    crypto_name = crypto.at(".cmc-table__cell--sort-by__symbol").content
+    crypto_price = crypto.at(".cmc-table__cell--sort-by__price").content
+    cryptos = {crypto_name => crypto_price}
+    cryptos_array << cryptos
+    print "."
+    sleep(0.08)
+  end
+  
+rescue NoMethodError
+  puts "loaded!"
+end
+
+print "#{cryptos_array}\n"
