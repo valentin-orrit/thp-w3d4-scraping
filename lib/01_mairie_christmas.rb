f6e08ffa7df857e0d61e0b4948078acb7ff36960
valentin-orrit-wsl <vorrit@gmail.com>
2024-04-12 11:20:27 +0200

    mod mairies

--- lib/01_mairie_christmas.rb
+++ lib/01_mairie_christmas.rb
@@ -6,7 +6,6 @@ begin
   doc = Nokogiri::HTML5(URI.open("https://www.aude.fr/annuaire-mairies-du-departement"))
   mairies = {}
   $mairies_array = []
-  # print "loading."
 
   doc.css(".directory-block__item").each do |id|
     ville = id.at(".directory-block__content").content.strip.split(' ')[1..-1].join(' ')
@@ -14,9 +13,6 @@ begin
 
     mairies = {ville => email}
     $mairies_array << mairies
-
-    # print "."
-    # sleep(0.08)
   end
 rescue NoMethodError
   puts "loaded!"
